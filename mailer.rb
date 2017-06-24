3e602e77de5317b4c5511deaa14f8447339263ae
Alan Denniston <alan@evil-admin.com>
2017-06-24 17:53:04 -0400

    mailer new features: random email, custom headers and attachments

--- mailer.rb
+++ mailer.rb
@@ -21,8 +21,11 @@ thrd = 1
 tls  = false
 usr  = nil
 pass = nil
+rdm  = nil
 auth = 'login'
 log  = '/tmp/mailer.log'
+head = Array.new
+att  = Array.new
 
 # Arguements
 opt_parser = OptionParser.new do |opts|
@@ -43,6 +46,9 @@ opt_parser = OptionParser.new do |opts|
   opts.on("-U", "--user username", "SMTP auth username", String) { |val| usr = val }
   opts.on("-P", "--pass username", "SMTP auth password", String) { |val| pass = val }
   opts.on("-o", "--helo host.domain.tld", "HELO string, ussually a hostname", String) { |val| helo = val }
+  opts.on("-d", "--header X-Header1 Value", "Comma seperated additional headers", Array) { |val| head = val } 
+  opts.on("-a", "--attach /path/to/file1.ext", "Comma seperated attachments", Array) { |val| att = val } 
+  opts.on("-r", "--random /path/to/messages", "Directory of random html and text messages.  This is typically used to load test a mail system.", String) { |val| rdm = val }
   opts.on("-L", "--tls", "Enable STARTTLS.") do
     tls = true
   end
@@ -114,13 +120,27 @@ end
 
 # Message files
 begin
-  msgstr  = File.read(text)
-  msghtml = File.read(html)
-rescue => e
-  puts "Error: #{e.message}"
-  logger.error("#{e.message.chomp}")
-  exit
-  logger.debug("Text File: #{text.chomp} HTML File: #{html.chomp}")
+  if rdm == nil
+    msgstr  = File.read(text)
+    msghtml = File.read(html)
+  else
+    puts "Random message directory: #{rdm}"
+    logger.info("Random message directory: #{rdm}")
+    tf = Dir["#{rdm}/*.txt"]
+    hf = Dir["#{rdm}/*.html"]
+    tf.concat hf
+    files = tf + hf
+    if files.empty?
+      puts "No files found to send."
+      logger.error("No files found to send.")
+      exit
+    end
+  end
+  rescue => e
+    puts "Error: #{e.message}"
+    logger.error("#{e.message.chomp}")
+    logger.debug("Text File: #{text.chomp} HTML File: #{html.chomp}")
+    exit
 end
 
 # Set from name if not set
@@ -145,7 +165,7 @@ if rcpt.empty?
   exit
 end
 
-# Let's send some email!
+# Setup threading
 puts "Begin processing send queue."
 logger.info("Begin processing send queue.")
 Parallel.each(rcpt, in_threads: thrd) do |user|
@@ -154,6 +174,30 @@ Parallel.each(rcpt, in_threads: thrd) do |user|
   lprcpt = "#{localpart.chomp} <#{user.chomp}>"
   lpfrm = "#{frnm.chomp} <#{frm.chomp}>"
   logger.debug("Beginning SMTP send from '#{lpfrm}' to '#{lprcpt.chomp}' with subject '#{sbj.chomp}'.")
+  # Are we sending random emails?
+  if rdm != nil
+    mess = files.sample.split('.')[0]
+    # I don't know how I want to handle random message subjects yet.
+    msgstr = ""
+    msghtml = ""
+    t = ""
+    if File.exists?("#{mess}.html")
+      t = File.open("#{mess}.html").grep(/title/)
+    end
+    if !t.empty?
+      sbj = t.to_s.gsub(/^.+?(<[^>]*>)|(<[^>]*>).+?$/, '')
+    else
+      s = mess.to_s.gsub('_', ' ')
+      sbj = s.to_s.gsub(/.*\//, '')
+    end
+    if File.exists?("#{mess}.txt")
+      msgstr = File.read("#{mess}.txt")
+    end
+    if File.exists?("#{mess}.html")
+      msghtml = File.read("#{mess}.html")
+    end
+  end
+  # Send email.
   begin
     mail = Mail.deliver do
       to      lprcpt
@@ -161,20 +205,36 @@ Parallel.each(rcpt, in_threads: thrd) do |user|
       subject sbj
       date    time.inspect
       header['X-Mailer'] = 'Evil Mailer'
-      text_part do
-        body msgstr
+      if !head.empty?
+        head.each do |hd|
+          header["#{hd.split[0]}"] = "#{hd.split[1]}"
+        end
       end
-      html_part do
-        content_type 'text/html; charset=UTF-8'
-        body msghtml
+      if !msgstr.empty?
+        text_part do
+          body msgstr
+        end
+      end
+      if !msghtml.empty?
+        html_part do
+          content_type 'text/html; charset=UTF-8'
+          body msghtml
+        end
+      end
+      if !att.empty?
+        att.each do |a|
+          add_file "#{a}"
+        end
       end
     end
     rescue => e
       puts "Unable send from #{frm.chomp} to #{user.chomp} - Error: \"#{e.message.chomp}\""
       logger.error("Unable send from #{frm.chomp} to #{user.chomp} - Error: \"#{e.message.chomp}\"")
   end
-  puts "Message sent from #{frm.chomp} to #{user.chomp} - Subject: \"#{sbj.chomp}\""
-  logger.info("Message sent from #{frm.chomp} to #{user.chomp} - Subject: \"#{sbj.chomp}\"")
+  if !defined?(e.message)
+    puts "Message sent from #{frm.chomp} to #{user.chomp} - Subject: \"#{sbj.chomp}\""
+    logger.info("Message sent from #{frm.chomp} to #{user.chomp} - Subject: \"#{sbj.chomp}\"")
+  end
 end
 
 # End
